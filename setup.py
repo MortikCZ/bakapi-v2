c05b7a9a4e7c5b85b199d39fe5e7ead4e27a8047
“MortikCZ” <Starec.Lukas@sosasou.cz>
2024-05-08 09:57:29 +0200

    Pypi Package fix

--- setup.py
+++ setup.py
@@ -6,21 +6,23 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='bakapiv2',
-    version='0.2',
+    name='bakapi-v2',
+    version='0.3',
     url='https://github.com/MortikCZ/bakapi-v2',
     project_urls = {
         "Documentation": "https://github.com/MortikCZ/bakapi-v2/blob/main/README.md",
         "Source Code": "https://github.com/MortikCZ/bakapi-v2/blob/main/bakapi-v2.py",
+        "License": "https://github.com/MortikCZ/bakapi-v2/blob/main/LICENSE",
         "Issues"    : "https://github.com/MortikCZ/bakapi-v2/issues"
     },
-    author='Lukáš S.',
+    author='Lukáš S., mwolfik',  
     description='Klient k API Bakalářů v jazyce Python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),    
     install_requires=['requests'],
     python_requires='>=3.6',
+    py_modules=["bakapiv2"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
